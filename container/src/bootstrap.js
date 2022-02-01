e6852d1d8c4f7d1c6dcd3a13bad779a2deedf795
omgpiu <dyba@list.ru>
2022-02-01 11:27:28 +0300

    finish

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,10 @@
 import {mount as productMount} from 'products/ProductsIndex';
 import {mount as cartMount} from 'cart/CartIndex';
 
+
+
+
+productMount(document.querySelector('#my-products'))
+productMount(document.querySelector('#my-products'))
 productMount(document.querySelector('#my-products'))
 cartMount(document.querySelector('#my-cart'))
